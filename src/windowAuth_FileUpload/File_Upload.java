398febfc66fadcd56a474075b9e49835a5159275
DHORASWAMI K <kdhoraswami@gmail.com>
2021-01-29 16:51:43 +0530

    Windows Authentication file upload

---
+++ src/windowAuth_FileUpload/File_Upload.java
@@ -0,0 +1,22 @@
+package windowAuth_FileUpload;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class File_Upload {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Work\\Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.ilovepdf.com/pdf_to_jpg");
+		driver.findElement(By.cssSelector("[class*='uploader__btn']")).click();
+		Thread.sleep(5000);
+		Runtime.getRuntime().exec("C:\\Work\\AutoIT\\File_Upload.exe");
+
+	}
+
+}
---
+++ src/windowAuth_FileUpload/Window_Auth.java
@@ -0,0 +1,19 @@
+package windowAuth_FileUpload;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Window_Auth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Work\\Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://admin:admin@the-internet.herokuapp.com/");
+		driver.findElement(By.linkText("Basic Auth")).click();
+
+	}
+
+}
