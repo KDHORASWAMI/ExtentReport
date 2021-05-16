4789f754a41aa70e683ea00bbc3df82064e032b3
DHORASWAMI K <kdhoraswami@gmail.com>
2021-05-16 12:29:13 +0530

    Line alignment

--- src/ActionClass.java
+++ src/ActionClass.java
@@ -15,7 +15,8 @@ public class ActionClass {
 		driver.get("https://www.amazon.in");
 		Actions a= new Actions(driver);
 		WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
-		a.moveToElement(driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"))).click().keyDown(Keys.SHIFT).sendKeys("Hello").doubleClick().build().perform();
+		a.moveToElement(driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")))
+			.click().keyDown(Keys.SHIFT).sendKeys("Hello").doubleClick().build().perform();
 		a.moveToElement(move).build().perform();
 		
 		
